0a3f421d90a98aa9ab5873ab5b9538ce32acae58
snyamson <snyamson@st.knust.edu.gh>
2023-09-21 15:45:27 +0000

    Complete app header component

--- src/app/app.py
+++ src/app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import date
 
 from utils.utils import override_app_padding
+from components.app_header import app_header
 
 
 # Apply the function to override default padding
@@ -9,3 +10,6 @@ override_app_padding()
 
 # Define custom color palette
 custom_color_palette = ["#835AF1", "#37AA9C", "#B8F7D4", "#94F3E4"]
+
+# Implement the app header
+view_trend_button = app_header()
