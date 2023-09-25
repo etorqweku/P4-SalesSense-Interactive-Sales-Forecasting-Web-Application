c1643d39a58c90903f4abb20552877895cd92af6
snyamson <snyamson@st.knust.edu.gh>
2023-09-25 09:31:56 +0000

    Complete utils refactoring to add comments

--- src/app/utils/data_preprocessor.py
+++ src/app/utils/data_preprocessor.py
@@ -2,21 +2,25 @@ import joblib
 import pandas as pd
 import numpy as np
 
+# Load the preprocessor and the XGBoost model
 preprocessor = joblib.load("/model/preprocessor.joblib")
 model = joblib.load("/model/xgb_model.joblib")
 
 
-# Define a function
+# Define a function to preprocess and predict sales
 def data_preprocessor(payload: dict):
     # Create a DataFrame from the payload with index 0
     payload_df = pd.DataFrame(payload, index=[0])
 
-    # Estimate X_min and X_max based on the original data
+    # Define the minimum and maximum values for scaling
     X_min = 0.00
     X_max = 952765.716
 
+    # Transform the input data using the preprocessor and make predictions
     scaled_value = model.predict(preprocessor.transform(payload_df))
 
-    # Unscaled value approximation to 3 decimal place
+    # Calculate the unscaled value based on the original range
     original_value = scaled_value * (X_max - X_min) + X_min
+
+    # Round the result to 3 decimal places
     return np.round(float(original_value[0]), 3)
